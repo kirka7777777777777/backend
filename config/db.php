daa5126809f6b34a323bc98a24b138ec97404a50
gr426_ivkma <ivanova.kiru2018@yandex.ru>
2025-05-14 11:50:09 +0700

    dasfdsa

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'mvc',
+    'username' => 'jfvntdac',
+    'password' => '9zsjjt',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
