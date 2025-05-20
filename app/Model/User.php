7bdbadf90b9361034e0b4817f060ad13a5f6220c
gr426_ivkma <ivanova.kiru2018@yandex.ru>
2025-05-20 10:54:11 +0700

    new

--- app/Model/User.php
+++ app/Model/User.php
@@ -4,8 +4,9 @@ namespace Model;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Src\Auth\IdentityInterface;
 
-class User extends Model
+class User extends Model implements IdentityInterface
 {
     use HasFactory;
 
@@ -16,12 +17,30 @@ class User extends Model
         'password'
     ];
 
-
     protected static function booted()
     {
         static::created(function ($user) {
             $user->password = md5($user->password);
             $user->save();
         });
+    }
+
+    //Выборка пользователя по первичному ключу
+    public function findIdentity(int $id)
+    {
+        return self::where('id', $id)->first();
+    }
+
+    //Возврат первичного ключа
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    //Возврат аутентифицированного пользователя
+    public function attemptIdentity(array $credentials)
+    {
+        return self::where(['login' => $credentials['login'],
+            'password' => md5($credentials['password'])])->first();
     }
 }
